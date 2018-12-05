601c33ac62b44aa479fab643da3326f76a3abc79
endertekin <etekin@wisc.edu>
2018-12-05 12:36:59 -0600

    Updated scripts & README

--- run-gpu.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-docker run -d --rm -p 8888:8888 -p 6006:6006 \
-    -v $(pwd)/private:/etc/notebook \
-    -v $(pwd)/notebooks:/tmp/notebooks \
-    -v /tmp/tflogs:/tmp/tflogs \
-    -w /tmp \
-    --runtime=nvidia \
-    --name tf-jupyter --init aviatr/tf_jupyter:gpu
